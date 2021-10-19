7ee6afd9472d3771402f2dee557643bc6ec24da8
DESKTOP-99GQ17G\Leti <trifanletitia5@gmail.com>
2021-10-19 09:01:27 +0300

    no message

--- main.py
+++ main.py
@@ -35,12 +35,17 @@ def siruricareserepeta(l):
 
 
 def test_siruricareserepeta():
-    assert (siruricareserepeta(['aaa', 'bbb', 'cmtc', 'drd', 'aaa'])==['aaa','bbb'])
+    assert siruricareserepeta(['aaa', 'bbb', 'cmtc', 'drd', 'aaa'])==['aaa','bbb']
 
 def isPalindrome(s):
     return s==s[::-1]
 
 def siruriPalindrom(l):
+    '''
+    Afiseaza sirurile de caractere care sunt palindrom
+    :param l: o lista care contine n siruri de caractere
+    :return: sirurile care sunt palindrom( o lista)
+    '''
     lista3=[]
     for x in l:
         ans=isPalindrome(x)
@@ -50,6 +55,8 @@ def siruriPalindrom(l):
 
 def test_siruriPalindrom():
     assert siruriPalindrom(['aaa','fgr','ggh'])==['aaa']
+
+
 def main():
     l = []
     #test_siruricareserepeta()
@@ -57,9 +64,9 @@ def main():
     while True:
         print("1. Citire lista ")
         print("2. Afisare  dacă un șir de caractere citit de la tastatură se găsește în listă")
-        print("3. ")
-        print("4. ")
-        print("5. ")
+        print("3. Afișarea listei cu toate șirurile de caractere care se repetă în listă ")
+        print("4. Afișarea tuturor șirurilor de caractere din lista care sunt un palindrom. ")
+        print("5. Afisarea listeiobtinute prin inlocuirea sirurilor care contin caracterul care apare de cele mai multe ori in toata lista cu numarul de aparitii ale acestui caracter")
         print("a. Afisare lista")
         print("x. Iesire")
 
